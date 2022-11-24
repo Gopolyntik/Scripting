c235dd2de25cb04822cc39c06d340eb6a4160604
Gopolyntik <118459228+Gopolyntik@users.noreply.github.com>
2022-11-24 19:58:17 +0500

    Add files via upload

---
+++ Nps.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nps : MonoBehaviour
+{
+    int hearth = 5;
+    int level = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hearth = level + hearth;
+      print()
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int health = 5;  
+    }
+}
